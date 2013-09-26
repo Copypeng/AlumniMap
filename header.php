6320b55a64ae1d6dfea27754ac2fe3d94dc8e3d8
copypeng <pengyongouc@gmail.com>
2013-09-26 20:25:14 +0800

    delete the sg

--- header.php
+++ header.php
@@ -5,12 +5,6 @@ include "./include/db.php";
 mysql_connect($db_host, $db_user, $db_pass) or die(mysql_error());
 mysql_select_db($db_name) or die(mysql_error());
 
-// if map is in Startup Genome mode, check for new data
-if($sg_enabled) {
-  require_once("include/http.php");
-  include_once("startupgenome_get.php");
-}
-
 // input parsing
 function parseInput($value) {
   $value = htmlspecialchars($value, ENT_QUOTES);
@@ -18,7 +12,5 @@ function parseInput($value) {
   $value = str_replace("\n", "", $value);
   return $value;
 }
-
-
 
 ?>
